2f15cbd101a833350dbe69e4ee50e42919b3e9a3
Mezashi <tzotzo840+github@gmail.com>
2024-12-26 17:53:24 +0630

    Updated

--- src/main/java/com/jlptpractice/Section1.java
+++ src/main/java/com/jlptpractice/Section1.java
@@ -1,18 +1,12 @@
 package com.jlptpractice;
 
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import static com.jlptpractice.GetStart.changeBG;
-import static com.jlptpractice.GetStart.changeScene;
 
 public class Section1 implements Initializable {
 
@@ -23,35 +17,8 @@ public class Section1 implements Initializable {
     public RadioButton answer3;
     public RadioButton answer4;
 
-    public void switchYM() throws Exception {
-        changeBG("YearMonth.jpg");
-        changeScene("YearMonth.fxml");
-    }
-
-    public void setLoader() {
-//        try {
-//            FXMLLoader loader = new FXMLLoader(getClass().getResource("Section1.fxml"));
-//            Node info_node = loader.load();
-////            questions_view.getChildren().add(info_node);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-//        setLoader();
         answer1.setText("ABC");
-//        setLoader();
-//        setLoader();
-//        setLoader();
-//        setLoader();
-//        setLoader();
-//        setLoader();
-//        setLoader();
-//        setLoader();
-//        setLoader();
-//        setLoader();
-//        setLoader();
     }
 }
