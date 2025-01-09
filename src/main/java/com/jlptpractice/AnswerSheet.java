14a01dedd6d9d0aa2bb54cd8dadb907fb778d8f2
Mezashi <tzotzo840+github@gmail.com>
2025-01-09 11:01:46 +0630

    Updated

--- src/main/java/com/jlptpractice/AnswerSheet.java
+++ src/main/java/com/jlptpractice/AnswerSheet.java
@@ -3,11 +3,33 @@ package com.jlptpractice;
 import com.jlptpractice.database.ManageDB;
 
 public class AnswerSheet {
-    static int correctAns;
 
-    static void setCorrectAns(){
-        for (String cs: ManageDB.correctAns()){
-            System.out.println(cs);
-        }
+    private final String correctAns1;
+    private final String correctAns2;
+    private final String correctAns3;
+    private final String correctAns4;
+
+    public AnswerSheet(String correctAns1, String correctAns2, String correctAns3, String correctAns4) {
+        this.correctAns1 = correctAns1;
+        this.correctAns2 = correctAns2;
+        this.correctAns3 = correctAns3;
+        this.correctAns4 = correctAns4;
     }
+
+    public String getCorrectAns1() {
+        return correctAns1;
+    }
+
+    public String getCorrectAns2() {
+        return correctAns2;
+    }
+
+    public String getCorrectAns3() {
+        return correctAns3;
+    }
+
+    public String getCorrectAns4() {
+        return correctAns4;
+    }
+
 }
